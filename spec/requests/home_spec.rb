1711fa8886247f6fadfed49286df41e7448a6024
Pavel Goloblia <iverick091@gmail.com>
2023-03-31 22:31:37 +0300

    Refactor existing code and add test cases

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,10 +1,22 @@
 require "rails_helper"
 
-RSpec.describe "Homes", type: :request do
+RSpec.describe "Home", type: :request do
   describe "GET /index" do
-    it "is successful" do
-      get root_path
-      expect(response).to have_http_status(:success)
+    context "when not signed in" do
+      it "is responds with redirect" do
+        get root_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+
+    context "when logged in" do
+      it "redirects to dashboard_path" do
+        user = create(:user)
+        sign_in user
+        get root_path
+        expect(response).to redirect_to(dashboard_path)
+      end
     end
   end
 end
