dcd5ed334490e3c4200f344468c8ff8957e58608
Pavel Goloblia <iverick091@gmail.com>
2023-04-13 14:31:23 +0300

    Add replies count to tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,7 +9,11 @@ class Tweet < ApplicationRecord
   has_many :retweeted_users, through: :retweets, source: :user
   has_many :views, dependent: :destroy
   has_many :viewed_users, through: :views, source: :user
-  belongs_to :parent_tweet, foreign_key: :parent_tweet_id, class_name: "Tweet", optional: true
+  belongs_to :parent_tweet,
+             foreign_key: :parent_tweet_id,
+             class_name: "Tweet",
+             optional: true,
+             counter_cache: :reply_tweets_count
   has_many :reply_tweets, foreign_key: :parent_tweet_id, class_name: "Tweet"
 
   validates :body, presence: true, length: { maximum: 280 }
