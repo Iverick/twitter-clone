c1c3afed15db3a2fe1fc40f46a1099dcd4205ff8
Pavel Goloblia <iverick091@gmail.com>
2023-04-23 20:29:15 +0300

    Fix bug with the following button

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -3,7 +3,7 @@ class FollowingsController < ApplicationController
 
   def create
     following = user.followings.create(following_params)
-
+    @follower = following.following_user
     respond_to do |format|
       format.html { redirect_to user_path(following.following_user) }
       format.turbo_stream
