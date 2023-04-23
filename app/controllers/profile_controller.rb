67e468937ee9c1b1cad50b98de3e79c954aef4b8
Pavel Goloblia <iverick091@gmail.com>
2023-04-23 22:49:24 +0300

    Functionality to display tweets on a user's page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,12 +2,15 @@ class ProfileController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
+    @tweets = user.tweets.map do |tweet|
+      TweetPresenter.new(tweet: tweet, current_user: user)
+    end
+    
     render "users/show"
   end
 
   def update
-    current_user.update(user_params[:password].blank? ? user_params.except(:password) : user_params)
+    user.update(user_params[:password].blank? ? user_params.except(:password) : user_params)
     respond_to do |format|
       format.html { redirect_to profile_path }
       format.turbo_stream
@@ -15,6 +18,10 @@ class ProfileController < ApplicationController
   end
 
   private
+
+  def user
+    @user ||= current_user
+  end
 
   def user_params
     params.require(:user).permit(:username, :display_name, :email, :password, :location, :bio, :profile_url)
