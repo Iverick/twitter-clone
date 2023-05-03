f1a1bc93fbb7e74a182362492b3cc901f0c19572
Pavel Goloblia <iverick091@gmail.com>
2023-05-03 23:42:02 +0300

    Creates hashtags on a tweet creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_24_194838) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_203301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,9 +78,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_24_194838) do
   end
 
   create_table "hashtags", force: :cascade do |t|
-    t.string "name"
+    t.string "tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["tag"], name: "index_hashtags_on_tag"
   end
 
   create_table "hashtags_tweets", id: false, force: :cascade do |t|
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -15,4 +15,34 @@ RSpec.describe Tweet, type: :model do
   it { should belong_to(:parent_tweet).with_foreign_key(:parent_tweet_id).class_name("Tweet").optional }
   it { should validate_presence_of(:body) }
   it { should validate_length_of(:body).is_at_most(280) }
+
+  describe "saving hashtags" do
+    let (:user) { create(:user) }
+
+    context "when there are no hashtags in the body" do
+      it "does not create any hashtags" do
+        expect do
+          Tweet.create(user: user, body: "no hashtags in the body")
+        end.not_to change { Hashtag.count }
+      end
+    end
+
+    context "when there are hashtags in the body" do
+      it "creates hashtags" do
+        expect do
+          Tweet.create(user: user, body: "no #hashtags in the #body")
+        end.to change { Hashtag.count }.by(2)
+      end
+    end
+
+    context "when there are duplicate hashtags in the body" do
+      it "does not create extra hashtags if already in the database table" do
+        Hashtag.create(tag: "hashtags")
+        expect do
+          Tweet.create(user: user, body: "no #hashtags in the #body")
+        end.to change { Hashtag.count }.by(1)
+      end
+    end
+
+  end
 end
